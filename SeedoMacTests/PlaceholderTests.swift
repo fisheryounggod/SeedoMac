8aec99b0fe838854a98dec6da96ab34fdfca012e
fisheryounggod <1632164730@qq.com>
2026-04-12 15:30:15 +0800

    chore: fix SWIFT_VERSION test target, remove plist duplication, add .gitignore

--- SeedoMacTests/PlaceholderTests.swift
+++ SeedoMacTests/PlaceholderTests.swift
@@ -2,6 +2,7 @@
 import XCTest
 
 final class PlaceholderTests: XCTestCase {
+    // TODO: Remove this placeholder once real tests are added in Task 4+
     func test_placeholder() {
         XCTAssertTrue(true)
     }
